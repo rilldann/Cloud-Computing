90f57f222d3b61d1c7fa100712102eed3babf154
Zidane Alfarizi <zidanealfarizi39@gmail.com>
2025-02-28 14:40:41 +0800

    3

---
+++ frontend/my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello from React + Vite!</h1>
+      <p>This is a simple React app built with Vite.</p>
+    </div>
+  );
+}
+
+export default App;
